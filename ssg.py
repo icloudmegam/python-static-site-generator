28726e7355ecf8331c9f40d390c54dd8f7dec1ca
Meganathan Padmanaban <Meganathan.Padmanaban@lennoxintl.com>
2021-05-23 00:05:15 +0530

    t

--- ssg.py
+++ ssg.py
@@ -1,8 +1,10 @@
 import typer
 from ssg.site import Site
 
-def main(soruce="content", dest="dist"):
+
+def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
     Site(**config).build()
+
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -1,20 +1,17 @@
-from pathlib import path 
+from pathlib import Path
 
-class Site():
-    def __init__(self,source,dest):
-        self.source = source
-        self.dest = dest
-        self.source = path()
-        self.dest = path()
 
-    def create_dir(self,path):
-        directory = self.dest / path.relativeto(self.source)
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build():
-        self.dest.mkdir(parents=True , exist_ok=True)
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if path.isdir():
+            if path.is_dir():
                 self.create_dir(path)
-            
-         
